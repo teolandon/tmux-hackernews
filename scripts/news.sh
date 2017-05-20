c08a2dfa92f6510bdd367711d0ad4261f1229dbc
Theo Kalfas <kalfastheodoros@gmail.com>
2017-05-20 03:08:32 -0500

    Links now cached properly

--- scripts/news.sh
+++ scripts/news.sh
@@ -8,20 +8,31 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 index=`. $DIR/compute_index.sh`
 
+# If headlines file exists and index is not 1, then just read from there
 if [ -r $DIR/../headlines.conf -a $index != 1 ]; then
-  echo `sed "${index}q;d" $DIR/../headlines.conf`
+  echo $index. `sed "${index}q;d" $DIR/../headlines.conf`
   exit
 fi
 
-headlines=$(curl -s https://news.ycombinator.com | grep "<a.*class=\"storylink\"" | sed -e "s/<td.*storylink\">\|<\/a.*\|<td align.*div>\|<td align.*nofollow\">//g" -e "s/^ *//g")
+# else, pull from news.ycombinator.com
 
+request=`curl -s https://news.ycombinator.com | grep "<a.*class=\"storylink\""`
+
+headlines=$(echo "$request" | sed -e "s/<td.*storylink\">\|<\/a.*\|<td align.*div>\|<td align.*nofollow\">//g" -e "s/^ *//g")
+
+# If nothing returns, then simply return NULL
 if [ -z "$headlines" ]
 then
   echo $headlines
   exit
 fi
 
+# links to append
+storylinks=$(echo "$request" | sed -e "s/.*<td class=\"title\"><a href=\"//g" -e "s/\" class=\"storylink\">.*//g")
+
+# Write to the headlines file
 echo "$headlines" > $DIR/../headlines.conf
+echo "$storylinks" >> $DIR/../headlines.conf
 
 headline=`sed "${index}q;d" $DIR/../headlines.conf`
 
--- scripts/urls.sh
+++ scripts/urls.sh
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 index=`. $DIR/compute_index.sh`
 
-storylink=`curl -s https://news.ycombinator.com | grep "<a.*class=\"storylink\"" | sed -e "s/.*<td class=\"title\"><a href=\"//g" -e "s/\" class=\"storylink\">.*//g" -e "${index}q;d"`
+storylink=`sed "${index}q;d" $DIR/../headlines.conf`
 
 echo $storylink
